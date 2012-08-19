b37adc5977c0e36c7c690b2918e578b6a2baa186
geta6 <getakura@gmail.com>
2012-08-20 00:25:12 +0900

    autopush

--- public/javascripts/direction.js
+++ public/javascripts/direction.js
@@ -2,7 +2,6 @@ $(function () {
   var direction = $('#direction');
   window.addEventListener('deviceorientation', function (e) {
     var compass = e.webkitCompassHeading;
-    direction.css({'-webkit-transform': 'rotate(' + (-1 * c) + 'deg)'});
-    console.log('DIR ', -1*c);
+    direction.css({'-webkit-transform': 'rotate(' + (-1 * compass) + 'deg)'});
   }, false);
 });
