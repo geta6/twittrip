4f9def74ec494c3180acdea9628d5cb4a28418e2
yuppon <aero.1322@gmail.com>
2012-08-20 08:53:35 +0900

    tube infoView

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -117,8 +117,6 @@ $(function () {
             }
           });
         }
-      }
-
         // after throw, stop rotate
         map_canvas.css({'-webkit-transform': 'rotate(' + (-1 * 0) + 'deg)'});
       }
