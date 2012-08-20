2b56b918b86f127bae7f29da0b4ca3ef0bae92be
yuppon <aero.1322@gmail.com>
2012-08-20 09:23:03 +0900

    tube infoView

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,6 @@
 var map;
 var mapOptions;
 var geocoder;
-var geocoder2;
 
 var currentLat;
 var currentLon;
@@ -16,7 +15,8 @@ var threw = false;
 $(function () {
 
   var str  = $('#string')
-    , map_canvas  = $('#map_canvas');
+    , map_canvas  = $('#map_canvas')
+    , dom_dir = $('#direction');
   var max = 0;
   var direction;
   var floatDistance;
@@ -26,26 +26,24 @@ $(function () {
   var floatingLocations;
 
   window.addEventListener('deviceorientation', function (e) {
-    var c = -1 * e.webkitCompassHeading
-      , a = e.webkitCompassAccuracy
-      , d = $('#direction');
+    var c = e.webkitCompassHeading
+      , a = e.webkitCompassAccuracy;
       //console.log(e.webkitCompassHeading);
     if(threw == false){
-      d.css({'-webkit-transform': 'rotate(' + c + 'deg)'});
+      dom_dir.css({'-webkit-transform': 'rotate(' + (-1 * c) + 'deg)'});
       heading = c;
     }
   }, false);
-
 
 
   window.addEventListener('devicemotion', function (e) {
     if(threw == false){//throw only once
       var tempLat = new Array(4);
       var tempLon = new Array(4);
+      infoWindow = [];
       infoPos = [];
       marker = [];
-      geocoder = [];
-      infoWindow = [];
+
       var g = Math.abs(e.accelerationIncludingGravity.x) + Math.abs(e.accelerationIncludingGravity.y) + Math.abs(e.accelerationIncludingGravity.z);
       if (20 < g) {
         max = (g > max) ? g : max;//適当に*10して単位はkm
@@ -55,16 +53,71 @@ $(function () {
         threw = true;
         max = 0;
       }
+
       movedDegree = floatDistance / 111;
       //set landing pos
       landingLat = Math.ceil(((currentLat + movedDegree) + movedDegree * Math.cos(heading * Math.PI/180))*100)/100;
       landingLon = Math.ceil((currentLon + movedDegree * Math.sin(heading * Math.PI/180))*100)/100;
+      // console.log("landing Pos:" + landingLat + ", " + landingLon);
+      console.log("moved" + movedDegree);
+      console.log("landing Pos:" + landingLat + ", " + landingLon);
+
+
+      //add marker
+      if(threw){
+        //add route to landing Pos
+        interval = Math.ceil((movedDegree / 5)*100)/100;
+
+
+        for(i=1;i<4;i++){
+          //liner function for the route
+          tempLat[0]=currentLat;
+          tempLon[0]=currentLon;
+          tempLat[4]=landingLat;
+          tempLon[4]=landingLon;
+          tempLat[i] = Math.ceil((tempLat[i-1]+interval)*100)/100;
+          tempLon[i] = (Math.ceil((((currentLon - landingLon)/(currentLat - landingLat)) * ((currentLat + interval)-currentLat)+currentLon)*100)/100)+Math.ceil(Math.random()*100)/100;
+        }
+         //alert(tempLon[3]);
+
+        //post to flickr
+        floatingLocations = [
+          ['current',tempLat[0],tempLon[0],1],
+          ['2',tempLat[1],tempLon[1],2],
+          ['3',tempLat[2],tempLon[2],3],
+          ['4',tempLat[3],tempLon[3],4],
+          ['landing',landingLat,landingLon,5]
+        ];
+
+        for(i=0;i<floatingLocations.length;i++){
+          var location = floatingLocations[i];
+          infoPos[i] = new google.maps.LatLng(tempLat[i],tempLon[i]);
+          marker[i] = new google.maps.Marker({
+            position: infoPos[i],
+            title: "twitTrip",
+            animation: google.maps.Animation.DROP,
+            icon: flag
+          });
+          marker[i].setMap(map);
+          geocoder.geocode({'latLng': infoPos[i]}, function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            if (results[5]) {
+              map.setZoom(3);
+              infoWindow[i] = new google.maps.InfoWindow({content: results[5].formatted_address});
+              alert(results[5].formatted_address);
+              //infoWindow[3].setContent("hoge");
+              infoWindow[3].open(map, marker[i]);
+            }
+          } else {
+            alert("Geocoder failed due to: " + status);
+          }
+        });
+        }
+        // after throw, stop rotate
+        map_canvas.css({'-webkit-transform': 'rotate(' + (-1 * 0) + 'deg)'});
+      }
     }
   }, false);
-
-
-
-
 
 
 
@@ -130,4 +183,3 @@ $(function(){
     map.setCenter(options.position);
   }
 });
-
